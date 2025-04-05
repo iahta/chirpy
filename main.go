0c0425d55f483076cf7fb4e7a1517a098c354971
iahta <153889016+iahta@users.noreply.github.com>
2025-04-05 14:50:35 -0500

    added /app

--- main.go
+++ main.go
@@ -3,8 +3,14 @@ package main
 import "net/http"
 
 func main() {
+	ok := []byte("OK")
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write(ok)
+	})
 
 	server := &http.Server{
 		Addr:    ":8080",
