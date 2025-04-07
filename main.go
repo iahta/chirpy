5757bb931f53372103d2aab2d653426ee4c17106
iahta <153889016+iahta@users.noreply.github.com>
2025-04-06 19:13:13 -0500

    added validation to request

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"sync/atomic"
 )
@@ -20,6 +22,7 @@ func main() {
 
 	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
 	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
+	mux.HandleFunc("POST /api/validate_chirp", apiCfg.validateHandler)
 
 	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -59,6 +62,49 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, "Counter Reset!")
+}
+
+func (cfg *apiConfig) validateHandler(w http.ResponseWriter, r *http.Request) {
+	type validate struct {
+		Body string `json:"body"`
+	}
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+
+	type validResponse struct {
+		Valid bool `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	val := validate{}
+	err := decoder.Decode(&val)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+
+		errResp := errorResponse{Error: "Something went wrong"}
+		dat, _ := json.Marshal(errResp)
+		w.Write(dat)
+		return
+	}
+	if len(val.Body) > 140 {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+
+		errResp := errorResponse{Error: "Chirp is too long"}
+		dat, _ := json.Marshal(errResp)
+		w.Write(dat)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	resp := validResponse{Valid: true}
+	dat, _ := json.Marshal(resp)
+	w.Write(dat)
 }
 
 /*
